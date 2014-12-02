d266d652213d9505663f54df3a3fc2026afa1624
Maria Klepikova <purr.effect@gmail.com>
2014-12-02 20:26:57 +0200

    fixed calendar url bug

--- cal/urls.py
+++ cal/urls.py
@@ -7,6 +7,6 @@ from cal import views
 
 urlpatterns = patterns('',
     url(r'^$', views.calendar, name='calendar'),
-    url(r'^(?P<mondayParam>.*)/$', views.calendar, name='calendar'),
+    url(r'^calendar/(?P<mondayParam>.*)/$', views.calendar, name='calendar'),
     
 )
