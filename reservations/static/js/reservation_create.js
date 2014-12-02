cc64f1bd3e35fd647a9bdd70523ceb12f8e446da
Maria Klepikova <purr.effect@gmail.com>
2014-12-02 21:00:18 +0200

    show/hide fields for customer in reservation

---
+++ reservations/static/js/reservation_create.js
@@ -0,0 +1,22 @@
+var CUSTOMERFIELDS = [
+  '#id_first_name',
+  '#id_last_name',
+  '#id_email',
+  '#id_street_address',
+  '#id_postcode',
+  '#id_city',
+  '#id_phone',
+  '#id_discount'
+];
+
+function showHide () {
+  var selectedCustomer = $('#id_customer').val();
+  if (selectedCustomer === '') {
+    $(CUSTOMERFIELDS.join(', ')).parent().show();
+  } else {
+    $(CUSTOMERFIELDS.join(', ')).parent().hide();
+  }
+}
+
+$('#id_customer').on('change', showHide);
+showHide();
