b64efc36c0d428d0ad3893b988a43d7c58cfb9d0
eugene-doe <dobryanskiy@mail.ru>
2015-01-15 15:57:13 +0300

    Calendar URLs fixed

--- cal/urls.py
+++ cal/urls.py
@@ -7,6 +7,7 @@ from cal import views
 urlpatterns = patterns('',
     url(r'^$', views.calendar, name='calendar'),
     url(r'^(?P<mondayParam>[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])/$', views.calendar, name='calendar'),
-      url(r'^(?P<mondayParam>[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])/(?P<coachParam>[0-9]*)$', views.calendar, name='calendar'),
+    url(r'^(?P<mondayParam>[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])/(?P<coachParam>[0-9]*)$', views.calendar, name='calendar'),
+    url(r'^(?P<coachParam>[0-9]*)$', views.calendar, name='calendar'),
     #url(r'^calendar/(?P<mondayParam>.*)/$', views.calendar, name='calendar'),
 )
